91d34c0b57aa7f4af84776d88a1270590c554301
Janek Zimoch <36931136+janekzimoch@users.noreply.github.com>
2019-12-11 06:57:52 +0000

    Add files via upload

---
+++ Romi_Mag_Z/Romi_Mag_Z.ino
@@ -0,0 +1,137 @@
+/*
+ * After beeping once, wave the robot around, rotate it,
+ * this will calibrate min/max values on the magnetometer.
+ * After the next beeps, leave the robot still on a flat
+ * surface, this will calibrate the gyro/accelerometer.
+ * 
+ * It should then be ready to go.
+ * 
+ */
+
+
+#include "encoders.h"   // setup and isr to manage encoders.
+#include "kinematics.h" // calculates x,y,theta from encoders.
+
+#include <Wire.h>
+#include "imu.h"
+#include "magnetometer.h"
+
+#define BUZZER_PIN      6   // To make the annoying beeping
+Kinematics  RomiPose;                                       // Not using ICC.
+Imu imu;
+Magnetometer mag; // Class for the magnetometer
+
+float gyro_z_angle = 0;
+float mag_z_angle = 0;
+
+// Complementary filters.
+float comp1 = 0;
+float comp1_alpha = 0.2;
+
+
+
+unsigned long update_t;
+
+void setup() {
+  
+  pinMode( BUZZER_PIN, OUTPUT );
+
+
+  Wire.begin();   
+
+  mag.init();
+  mag.calibrate();
+
+  imu.init();
+  imu.calibrate();
+
+  // Start up the serial port.
+  Serial.begin(9600);
+  delay(1000);
+
+  
+  // Begin tracking encoder changes.
+  setupEncoder0();
+  setupEncoder1();
+
+  update_t = millis();
+  delay(1); // stop dt being = 0
+
+  // First sensor update
+  updateSensors();
+
+  // The magnetometer has a fixed value based
+  // on the earths magnetic field, so lets set
+  // the gyro and kinematics to begin matching
+  // the magnetometer instead.
+  gyro_z_angle = mag_z_angle;
+  RomiPose.setPose(0,0, gyro_z_angle);
+  
+  Serial.println("Starting:");
+  Serial.print( mag_z_angle );
+  Serial.print(",");
+  Serial.print( gyro_z_angle );
+  Serial.print(",");
+  Serial.println( RomiPose.theta );
+}
+
+// Function wraps up the gyro, acceleromter (IMU) and
+// magnetometer reads.  
+void updateSensors() {
+  
+  long diff = ( millis() - update_t);
+  update_t = millis();
+  float dt = (float)diff;
+  dt /= 1000;
+
+  imu.readCalibrated();
+  mag.readCalibrated();
+
+  // Scale factor to rads - I found this by testing
+  // it is still a bit off.  I've been adjusting the 
+  // whole number to get better accuracy.
+  float rad_scale = 900 *15.7;
+  
+  gyro_z_angle -= ((imu.gz / (rad_scale) ) * dt );
+
+  
+  // Extracted from: http://www.freescale.com/files/sensors/doc/app_note/AN4248.pdf
+  // Note that, assuming the Romi is flat, therefore pitch and roll both = 0
+  // So I cancelled out most the terms
+  // Left in the variable names so we can find it in the doc.
+  // I'm not actually sure why this works so well - but there are still some
+  // problems to solve.
+  float Bfy = mag.cy;
+  float Bfx = mag.cx;
+  mag_z_angle = atan2( Bfy, Bfx );
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  RomiPose.update(e0_count, e1_count);
+  
+  updateSensors();
+
+  // lets attempt combining the gyro and mag with
+  // a simple complementary filter.
+  // Basically, which do we trust more?
+  // No prediction here, so no adjustment of alpha.
+  comp1 = ( mag_z_angle * comp1_alpha) + ( gyro_z_angle * (1-comp1_alpha)  );
+
+  
+  
+  Serial.print( comp1 );
+  Serial.print(",");
+  Serial.print( mag_z_angle );
+  Serial.print(",");
+  Serial.print( gyro_z_angle );
+  Serial.print(",");
+  Serial.println( RomiPose.theta );
+  
+  delay(25);
+
+  
+
+}
+
---
+++ Romi_Mag_Z/imu.h
@@ -0,0 +1,128 @@
+#ifndef _IMU_h
+#define _IMU_h
+
+#include <LSM6.h>
+#define BUZZER_PIN      6   // To make the annoying beeping
+const int NUM_CALIBRATIONS_IMU = 50;
+
+class Imu
+{
+    public:
+        void init();
+        void  readRaw();
+        void  readCalibrated();
+        void  calibrate();
+        LSM6 imu;
+        float ax = 0;
+        float ay = 0;
+        float az = 0;
+        float gx = 0;
+        float gy = 0;
+        float gz = 0;
+
+    private:
+        float a_sensitivity = 0.061;
+        float g_sensitivity = 8.75;
+        float gx_offset = 0;
+        float gy_offset = 0;
+        float gz_offset = 0;
+
+
+};
+
+void Imu::init()
+{
+    if (!imu.init())
+    {
+        Serial.println("Failed to detect and initialize magnetometer!");
+        while (1);
+    }
+
+    imu.enableDefault();
+
+  // I found the two below lines in another piece of
+  // code using the LSM6.
+
+  // Set the gyro full scale to 1000 dps because the default
+  // value is too low, and leave the other settings the same.
+  imu.writeReg(LSM6::CTRL2_G, 0b10001000);
+
+  // Set the accelerometer full scale to 16 g because the default
+  // value is too low, and leave the other settings the same.
+  imu.writeReg(LSM6::CTRL1_XL, 0b10000100);
+}
+
+void Imu::readRaw()
+{
+  
+  imu.read();
+  
+  gx = imu.g.x;
+  gy = imu.g.y;
+  gz = imu.g.z;
+
+  ax = imu.a.x;
+  ay = imu.a.y;
+  az = imu.a.z;
+  
+}
+
+void Imu::readCalibrated()
+{
+
+  imu.read();
+  
+  gx = g_sensitivity * (imu.g.x - gx_offset);
+  gy = g_sensitivity * (imu.g.y - gy_offset);
+  gz = g_sensitivity * (imu.g.z - gz_offset);
+
+  ax = a_sensitivity * imu.a.x;
+  ay = a_sensitivity * imu.a.y;
+  az = a_sensitivity * imu.a.z;
+
+ 
+ 
+  
+}
+
+void Imu::calibrate()
+{
+   analogWrite(BUZZER_PIN, 10);
+  delay(50);
+  digitalWrite( BUZZER_PIN, LOW );
+  delay(500);
+  analogWrite(BUZZER_PIN, 10);
+  delay(50);
+  digitalWrite( BUZZER_PIN, LOW );
+  delay(500);
+  for (int i=0;i<NUM_CALIBRATIONS_IMU; i++)
+  {
+    //analogWrite(BUZZER_PIN, 10);
+    delay(5);
+    
+    imu.read();
+
+    gx_offset += ((float)imu.g.x / NUM_CALIBRATIONS_IMU);
+    gy_offset += ((float)imu.g.y / NUM_CALIBRATIONS_IMU);
+    gz_offset += ((float)imu.g.z / NUM_CALIBRATIONS_IMU);
+    analogWrite(BUZZER_PIN, 0);
+    delay(50);
+  }
+
+   analogWrite(BUZZER_PIN, 10);
+  delay(50);
+  digitalWrite( BUZZER_PIN, LOW );
+  delay(50);
+  analogWrite(BUZZER_PIN, 10);
+  delay(50);
+  digitalWrite( BUZZER_PIN, LOW );
+  delay(50);
+
+  
+}
+
+
+
+
+#endif
+
